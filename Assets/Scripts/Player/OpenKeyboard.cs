2cf266c1bbbc4ffe932f2173e53730b7b17ebdcf
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-06-29 22:14:24 +0900

    change dirs

---
+++ Assets/Scripts/Player/OpenKeyboard.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRKeys;
+
+public class OpenKeyboard : MonoBehaviour
+{
+    Keyboard keyboard;
+    private bool isKeyboardOpen = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        keyboard = GetComponent<Keyboard>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool IsButtonXPressed = OVRInput.GetDown(OVRInput.Button.Three);
+        if (IsButtonXPressed)
+        {
+            isKeyboardOpen = !isKeyboardOpen;
+            if (isKeyboardOpen)
+            {
+                keyboard.Enable();
+            }
+            else
+            {
+                keyboard.Disable();
+            }
+        }
+    }
+}
