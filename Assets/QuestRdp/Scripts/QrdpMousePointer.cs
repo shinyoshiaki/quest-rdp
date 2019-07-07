026d3cb264b5ac3d5ded8ccbac98de3c742ae15c
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-07-07 16:22:20 +0900

    rm

---
+++ Assets/QuestRdp/Scripts/QrdpMousePointer.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+public class QrdpMousePointer : MonoBehaviour
+{
+    public GameObject reticle;
+
+    [SerializeField]
+    Transform grip = null;
+
+    [SerializeField]
+    float maxGrabDistance = 10f;
+
+    [SerializeField]
+    LayerMask layerMask = ~0;
+
+    Vector3 preRayDirection_;
+
+    RaycastHit targetHit_;
+
+    GameObject pos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = new GameObject();
+        Observable.Interval(TimeSpan.FromMilliseconds(1000 / 30)).Subscribe(l =>
+        {
+            var pos = DesktopRay();
+            if (pos.mouse)
+                pos.mouse.Move(pos.x, pos.y);
+        }).AddTo(this);
+        reticle = Instantiate(reticle);
+    }
+
+    public Transform gripTransform
+    {
+        get { return grip ? grip : transform; }
+    }
+
+
+    // Update is called once per frame
+    (float x, float y, QrdpMouseControll mouse) DesktopRay()
+    {
+        float x = 0; float y = 0;
+        QrdpMouseControll mouse = null;
+
+        var forward = gripTransform.forward;
+
+        var ray = new Ray();
+        ray.origin = gripTransform.position;
+        ray.direction = Vector3.Lerp(preRayDirection_, forward, 0.25f);
+
+        targetHit_ = new RaycastHit();
+        bool hit = Physics.Raycast(ray, out targetHit_, maxGrabDistance, layerMask);
+
+        if (hit)
+        {
+            var obj = targetHit_.collider.gameObject;
+            mouse = obj.GetComponent<QrdpMouseControll>();
+            if (mouse)
+            {
+                var hitPoint = new Vector2(targetHit_.textureCoord.x, targetHit_.textureCoord.y);
+                x = hitPoint.x;
+                y = hitPoint.y;
+
+                reticle.transform.rotation = Quaternion.LookRotation(targetHit_.normal);
+                reticle.transform.position = targetHit_.point + (-targetHit_.normal * 0.01f);
+
+            }
+        }
+
+        preRayDirection_ = hit ? ray.direction : forward;
+
+        return (x, y, mouse);
+    }
+
+}
---
+++ Assets/VRKeys/Scripts/LetterKey.cs
@@ -0,0 +1,48 @@
+﻿/**
+ * Copyright (c) 2017 The Campfire Union Inc - All Rights Reserved.
+ *
+ * Licensed under the MIT license. See LICENSE file in the project root for
+ * full license information.
+ *
+ * Email:   info@campfireunion.com
+ * Website: https://www.campfireunion.com
+ */
+
+using UnityEngine;
+using System.Collections;
+using TMPro;
+
+namespace VRKeys {
+
+	/// <summary>
+	/// An individual letter key.
+	/// </summary>
+	public class LetterKey : Key {
+		public TextMeshPro shiftedLabel;
+
+		public string character = "";
+
+		public string shiftedChar = "";
+
+		private bool _shifted = false;
+
+		public bool shifted {
+			get { return _shifted; }
+			set {
+				_shifted = value;
+				label.text = _shifted ? shiftedChar : character;
+				shiftedLabel.text = _shifted ? character : shiftedChar;
+			}
+		}
+
+		public string GetCharacter () {
+			return _shifted ? shiftedChar : character;
+		}
+
+		public override void HandleTriggerEnter (Collider other) {
+			keyboard.AddCharacter (GetCharacter ());
+
+			ActivateFor (0.3f);
+		}
+	}
+}
